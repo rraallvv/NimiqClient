9734295ad9a5c4f59d51c39a51662cd2b5cf578a
Developer <>
2020-07-13 02:09:21 -0400

    add package managers

---
+++ NimiqJSONRPCClient/Pods/Target Support Files/Pods-NimiqJSONRPCClientiOSTests/Pods-NimiqJSONRPCClientiOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NimiqJSONRPCClientiOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_NimiqJSONRPCClientiOSTests
+@end
---
+++ NimiqJSONRPCClient/Pods/Target Support Files/Pods-NimiqJSONRPCClientmacOSTests/Pods-NimiqJSONRPCClientmacOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NimiqJSONRPCClientmacOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_NimiqJSONRPCClientmacOSTests
+@end
