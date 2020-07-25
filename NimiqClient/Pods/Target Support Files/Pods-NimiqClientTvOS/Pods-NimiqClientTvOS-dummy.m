a6690beb9504fbae73fc173decc71a84c3dfc150
Developer <>
2020-07-25 10:47:57 -0400

    change to SPM project

--- NimiqClient/Pods/Target Support Files/Pods-NimiqClientTvOS/Pods-NimiqClientTvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NimiqClientTvOS : NSObject
-@end
-@implementation PodsDummy_Pods_NimiqClientTvOS
-@end
--- NimiqClient/Pods/Target Support Files/Pods-NimiqClientTvOSTests/Pods-NimiqClientTvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NimiqClientTvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_NimiqClientTvOSTests
-@end
